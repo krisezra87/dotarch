736c1cb5b73f92480617e87ccf8faf86360cb468
Kris Ezra <kris.ezra@gmail.com>
2019-09-28 00:12:18 -0400

    Adding installers

---
+++ configureArch.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Do some mad symlinking
+ln -sf ~/.arch/xinitrc ~/.xinitrc
+ln -sf ~/.arch/xprofile ~/.xprofile
+ln -sf ~/.arch/Xresources ~/.Xresources
+ln -sf ~/.arch/config/compton.conf ~/.config/compton.conf
+ln -sf ~/.arch/config/dwm ~/.config/dwm
+ln -sf ~/.arch/config/i3 ~/.config/i3
+ln -sf ~/.arch/config/qutebrowser ~/.config/qutebrowser
+ln -sf ~/.arch/config/zathura ~/.config/zathura
