59c81e79184d1d4fe6f7de5c7eec52ddffd0d523
Kris Ezra <kris.ezra@gmail.com>
2019-12-19 17:37:36 -0500

    Updated configuration to include sxiv, vifm, and picom

--- configureArch.sh
+++ configureArch.sh
@@ -9,3 +9,5 @@ ln -sf ~/.arch/config/dwm ~/.config/dwm
 ln -sf ~/.arch/config/i3 ~/.config/i3
 ln -sf ~/.arch/config/qutebrowser ~/.config/qutebrowser
 ln -sf ~/.arch/config/zathura ~/.config/zathura
+ln -sf ~/.arch/config/sxiv ~/.config/sxiv
+ln -sf ~/.arch/config/vifm ~/.config/vifm
