f6da1242ce5ccc16cca79fd61066e017312b21ab
Kris Ezra <kris.ezra@gmail.com>
2020-08-11 23:28:16 -0400

    Clean up install of dwm (new scripts folder) and add scripts to path by default

--- configureArch.sh
+++ configureArch.sh
@@ -5,8 +5,6 @@ ln -sf ~/.arch/xinitrc ~/.xinitrc
 ln -sf ~/.arch/xprofile ~/.xprofile
 ln -sf ~/.arch/Xresources ~/.Xresources
 ln -sf ~/.arch/config/picom.conf ~/.config/picom.conf
-ln -sf ~/.arch/config/dwm ~/.config/dwm
-ln -sf ~/.arch/config/i3 ~/.config/i3
 ln -sf ~/.arch/config/qutebrowser ~/.config/qutebrowser
 ln -sf ~/.arch/config/zathura ~/.config/zathura
 ln -sf ~/.arch/config/sxiv ~/.config/sxiv
